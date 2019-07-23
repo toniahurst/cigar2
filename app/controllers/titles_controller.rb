82576c6061f2ac65bf00618229f0f920d4a6d1b9
toniahurst <antonia.a.hurst@gmail.com>
2019-07-23 21:47:08 +0000

    added simple form. Things not connected properly yet.

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -2,4 +2,19 @@ class TitlesController < ApplicationController
 	def index
 		@titles = Title.all
 	end
+
+	def new
+		@title = Title.new
+	end
+
+	def create
+		Title.create(title_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def title_params
+		params.require(:title).permit(:name, :author, :description)
+	end
 end
